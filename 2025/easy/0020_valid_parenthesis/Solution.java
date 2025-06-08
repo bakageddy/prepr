1e5dabc532c985e1186e22f5c3609d542e036819
Dinesh Kumar <gdineshda@gmail.com>
2025-06-08 20:49:15 +0530

    Something somethign

---
+++ 2025/easy/0020_valid_parenthesis/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class Solution {
+	public boolean isValid(String s) {
+		Stack<Character> st = new Stack<>();
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+		}
+		return true;
+	}
+}
