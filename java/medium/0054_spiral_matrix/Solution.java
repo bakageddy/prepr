b74eae3446c816b20fa24624e144ca945db6dd0a
bakageddy <gdineshda@gmail.com>
2025-08-03 22:10:28 +0530

    0054

---
+++ java/medium/0054_spiral_matrix/Solution.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Solution {
+	enum Direction {
+		LEFT,
+		RIGHT,
+		UP,
+		DOWN
+	}
+    public List<Integer> spiralOrder(int[][] matrix) {
+		int m = matrix.length;
+		int n = matrix[0].length;
+		List<Integer> list = new ArrayList<>();
+		Direction dir = Direction.RIGHT;
+		return null;
+    }
+}
